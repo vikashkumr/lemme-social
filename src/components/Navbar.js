f29630d6caf6f4a3cb9ab7081d6066d2adcdd81b
vikashkumr <vky0018@gmail.com>
2020-04-28 01:44:53 +0530

    Routes update and skeleton designed for UI

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../App.css'
+import { Link } from 'react-router-dom';
+const NavBar = () => {
+    return (
+        <nav>   
+          <div className="nav-wrapper white" >
+          <Link to="#" className="brand-logo">LeMme`Social</Link>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+              <li><Link to="/profile">Profile </Link></li>
+              <li><Link to="/login">Login </Link></li>
+              <li><Link to="/signup">SignUp </Link></li>
+          </ul>
+          </div>
+        </nav>
+    );
+}
+
+export default NavBar;
